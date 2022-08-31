f1cde7aed2dd5e626bade6268a672485733d106d
Alejandro Montoya R <amontoyar@unal.edu.co>
2022-08-30 19:22:56 -0500

    Rta punto 10 e import itemgetter

--- preguntas.py
+++ preguntas.py
@@ -14,6 +14,7 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 import itertools
 from collections import Counter
 from datetime import datetime
+from operator import itemgetter
 
 with open ("data.csv", "r") as file:
         datos = file.readlines()
@@ -294,7 +295,15 @@ def pregunta_10():
 
 
     """
-    return
+    columnas = [itemgetter(0,3,4)(i) for i in datos]
+    conteo = []
+    for x in columnas:
+
+        col4 = len(x[1].split(","))
+        col5 = len(x[2].split(","))
+        conteo.append((x[0],col4,col5))
+
+    return conteo
 
 
 def pregunta_11():
